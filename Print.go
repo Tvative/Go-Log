e2791a390ba747b65ececc9e89df1be56bef06b3
Lasan Nishshanka <19279535+las-nish@users.noreply.github.com>
2023-10-20 18:53:51 +0530

    Delete Print.go

--- Print.go
+++
@@ -1,9 +0,0 @@
-// Terminal Text Generation
-//
-// Copyright (c) 2023 Tvative
-// All Rights Reserved
-//
-// Use of this source code is governed by
-// certain licenses found in the LICENSE file
-
-package GoLog
