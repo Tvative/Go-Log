f28ec9c02bd0c79dd62ebe5287d789c9ddad4cb0
Lasan Nishshanka <19279535+las-nish@users.noreply.github.com>
2023-10-20 18:39:32 +0530

    Add fatal print functions

--- Fatal.go
+++ Fatal.go
@@ -7,3 +7,21 @@
 // certain licenses found in the LICENSE file
 
 package GoLog
+
+// FatalP logs a message to the terminal with fatal formatting
+
+func (logData LogData) FatalP(messageContent string) {
+	logData.printOutPut(false, true, false, MessageFatal, messageContent)
+}
+
+// FatalPC logs a message to the terminal with fatal formatting
+
+func (logData LogData) FatalPC(messageContent string) {
+	logData.printOutPut(false, true, true, MessageFatal, messageContent)
+}
+
+// FatalF logs a fatal message to the log file
+
+func (logData LogData) FatalF(messageContent string) {
+	logData.printOutPut(true, false, false, MessageFatal, messageContent)
+}
