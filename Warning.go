bccedff7f51e8a530b3abb2fa4a8111ab6572f3c
Lasan Nishshanka <19279535+las-nish@users.noreply.github.com>
2023-10-20 19:34:58 +0530

    Add warning log message

--- Warning.go
+++ Warning.go
@@ -7,3 +7,21 @@
 // certain licenses found in the LICENSE file
 
 package GoLog
+
+// WarningP logs a message to the terminal with warning formatting
+
+func (logData LogData) WarningP(messageContent string) {
+	logData.printOutPut(false, true, false, MessageWarning, messageContent)
+}
+
+// WarningPC logs a message to the terminal with warning formatting
+
+func (logData LogData) WarningPC(messageContent string) {
+	logData.printOutPut(false, true, true, MessageWarning, messageContent)
+}
+
+// WarningF logs a warning message to the log file
+
+func (logData LogData) WarningF(messageContent string) {
+	logData.printOutPut(true, false, false, MessageWarning, messageContent)
+}
