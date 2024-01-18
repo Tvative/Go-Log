148c6913ffc94424ae6f593b590ed059b13af3fb
Lasan Nishshanka <19279535+las-nish@users.noreply.github.com>
2024-01-18 19:58:35 +0530

    Re-structure log package

---
+++ Test/test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/Tvative/Package-Go-Log"
+)
+
+var instance *golog.Instance
+
+func main() {
+	instance = golog.Initialize()
+
+	instance.SetFile("Test/test.log")
+	instance.SetFileFormat(golog.DefaultFormat)
+	instance.SetTerminalFormat(golog.DefaultFormat)
+
+	var moreJson = map[string]interface{}{
+		"Sample": "Content",
+	}
+
+	var testJson = map[string]interface{}{
+		"Sample": "Content",
+		"More":   moreJson,
+	}
+
+	instance.Log(testJson, "Default Log")
+	instance.Success(testJson, "Success Log")
+	instance.Error(testJson, "Error Log")
+	instance.Warning(testJson, "Warning Log")
+	instance.Debug(testJson, "Debug Log")
+	instance.Information(testJson, "Information Log")
+	instance.Fatal(testJson, "Fatal Log")
+}
